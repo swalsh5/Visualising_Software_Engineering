f5f37b54b3f3a35c2307851884a27262e12542c7
Sam <sam.walsh31@gmail.com>
2020-12-17 16:32:11 +0000

    First GitHub API functionality added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+const btnRepos = document.getElementById("btnRepos")
+const divResult = document.getElementById("divResult")
+btnRepos.addEventListener("click", getRepos)
+
+async function getRepos() {
+    const url = "https://api.github.com/search/repositories?q=stars:>100000"
+    const response = await fetch(url)
+    const result = await response.json()
+
+    result.items.forEach(i => {
+        divResult.appendChild(document.createTextNode(i.full_name))
+        divResult.appendChild(document.createElement("br"))
+    } )
+}
