8b6ef381676c3cfc6dc85354ee9a20ba26ad92ac
Sam <sam.walsh31@gmail.com>
2021-01-01 19:50:54 +0000

    Began adding main js functionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,117 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your Javascript code.
+// Handle Input ----------------------------------------------------------------------------
 
-const btnRepos = document.getElementById("btnRepos")
-const btnIssues = document.getElementById("btnIssues")
-const btnCommits = document.getElementById("btnCommits")
-const btnIssuesPrivate = document.getElementById("btnIssuesPrivate")
-
-const divResult = document.getElementById("divResult")
-
-btnRepos.addEventListener("click", getRepos)
-btnIssues.addEventListener("click", getIssues)
-btnCommits.addEventListener("click", e => getCommits())
-btnIssuesPrivate.addEventListener("click", e =>  getIssuesPrivate())
-
-async function getRepos() {
-    clear()
-    const url = "https://api.github.com/search/repositories?q=stars:>100000"
-    const response = await fetch(url)
-    const result = await response.json()
-
-    result.items.forEach(i => {
-        const anchor = document.createElement("a")
-        anchor.href = i.html_url;
-        anchor.textContent = i.full_name;
-        divResult.appendChild(anchor)
-        divResult.appendChild(document.createElement("br"))
-    })
+function handleInput() {
+    var user = document.getElementById("textBox").value;
+    main(user);
 }
 
-async function getIssues() {
-    clear()
-    const url = "https://api.github.com/search/issues?q=author:najmieh repo:freecodecamp/freecodecamp type:issue"
-    const response = await fetch(url)
-    const result = await response.json()
+// Get Request ------------------------------------------------------------------------------
 
-    result.items.forEach(i => {
-        const anchor = document.createElement("a")
-        anchor.href = i.html_url;
-        anchor.textContent = i.title;
-        divResult.appendChild(anchor)
-        divResult.appendChild(document.createElement("br"))
-    })
+async function GetRequest(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    return data;
 }
 
-async function getCommits(url = "https://api.github.com/search/commits?q=repo:freecodecamp/freecodecamp author-date:2020-09-01..2020-09-30") {
-    clear(); 
-    const headers = {
-        "Accept": "application/vnd.github.cloak-preview"
-    }
-    const response = await fetch(url, {
-        "method" : "GET",
-        "headers": headers
-    })
+// Main -----------------------------------------------------------------------------------
 
-    const link = response.headers.get("link")
-    const links = link.split(",")
-    const urls = links.map(a => {
-        return {
-            url: a.split(";")[0].replace(">","").replace("<",""),
-            title: a.split(";")[1]
-        }
-    })
+async function main(user) {
+    let url = 'https://api.github.com/users/' + user + '/repos';
+    let repoData = await GetRequest(url).catch(error => console.error(error))
 
-    const result = await response.json()
+    //User Stats
+    AvgCommits(repoData);
+    FavouriteLanguage(repoData);
+    MostCommits(repoData, user);
 
-    result.items.forEach(i => {
-        const img = document.createElement("img")
-        img.src = i.author.avatar_url;
-        img.style.width = "32px"
-        img.style.height = "32px"
+    //Social Graph
+    parseSocialGraph(repoData);
+    //PieChart
+    languageChart(repoData, user)
+    //Line Graph
+    parseLineGraph(repoData);
 
-        const anchor = document.createElement("a")
-        anchor.href = i.html_url;
-        anchor.textContent = i.commit.message.substr(0,120) + "...";
-
-        divResult.appendChild(img)
-        divResult.appendChild(anchor)
-        divResult.appendChild(document.createElement("br"))
-    })
-
-    urls.forEach(u => {
-        const btn = document.createElement("button")
-        btn.textContent = u.title;
-        btn.addEventListener("click", e => getCommits(u.url))
-        divResult.appendChild(btn);
-    })
 }
 
- // ff37ca4d426b67f55d7b7f8809dfd59d1b614731
-async function getIssuesPrivate() {
-    clear()
-    const headers = {
-        "Authorization": 'Token ff37ca4d426b67f55d7b7f8809dfd59d1b614731'
-    }
-    const url = "https://api.github.com/search/issues?q=repo:swalsh5/lca_java type:issue"
-    const response = await fetch(url, {
-        "method": "GET",
-        "headers": headers
-    })
-    const result = await response.json()
-
-    result.items.forEach(i => {
-        const anchor = document.createElement("a")
-        anchor.href = i.html_url;
-        anchor.textContent = i.title;
-        divResult.appendChild(anchor)
-        divResult.appendChild(document.createElement("br"))
-    })
-}
-
-function clear() {
-    while (divResult.firstChild)
-        divResult.removeChild(divResult.firstChild)
-}
